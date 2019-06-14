c740537252b63173e73c6364b884e92490452ed8
Myles Mierswa <mylesmierswa@gmail.com>
2019-06-14 13:38:32 -0400

    ex 46

---
+++ ex46/projects/ex46/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Myles Mierswa',
+    'url': 'the url',
+    'download_url': 'download url',
+    'author_email': 'myles.mierswa@test.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex46'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
